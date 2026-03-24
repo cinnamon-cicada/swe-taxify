cd725f61efc6a7052acc91e420f1230d5aeab005
cinnamon-cicada <eluo.5230@gmail.com>
2026-03-24 16:24:19 +0100

    add initial provided files

---
+++ sprint3/TestProgram.java
@@ -0,0 +1,11 @@
+package sprint3;
+
+// Declare a list of users. Instantiate at least 15 users.
+// Declare a list of vehicles. Instantiate at least 10 vehicles (Taxis and Shuttles) and locate them at random locations of the city map.
+// Instantiate the taxi company and the application simulator. Add the application simulator as an observer of the taxy company.
+// The simulation begins with at least 5 requests of service, and ends when all vehicles end their services. Show the status of the application each iteration and update its state. Simulate a request of a service randomly, to avoid requesting a service each iteration.
+// Finally, show the statistics.
+
+public class TestProgram {
+    
+}
