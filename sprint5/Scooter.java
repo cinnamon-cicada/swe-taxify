95b7e3d30c54d0e97e8483773d63600a84e54dd7
cinnamon-cicada <eluo.5230@gmail.com>
2026-04-19 14:04:24 -0500

    Add javadoc

--- sprint5/Scooter.java
+++ sprint5/Scooter.java
@@ -3,6 +3,13 @@ package sprint5;
 public class Scooter extends Vehicle {
     private double battery;  // Battery level from 0 to 100 (percentage)
 
+    /**
+     * Constructs a Scooter with the given ID, location, driver, and company.
+     * @param id the vehicle ID
+     * @param location the initial location
+     * @param driver the assigned driver
+     * @param company the taxi company this scooter belongs to
+     */
     public Scooter(int id, ILocation location, IDriver driver, ICompany company) {
         super(id, location, driver);
         this.setCompany(company);
@@ -16,6 +23,10 @@ public class Scooter extends Vehicle {
         return battery;
     }
 
+    /**
+     * Sets the battery level for this scooter.
+     * @param battery the battery level to set (0-100)
+     */
     @Override
     public double calculateCost() {
         return (super.calculateCost() - 1) * .15 + 1; // Scooters have a base cost of 1, and then charge 15% of the distance cost above that
